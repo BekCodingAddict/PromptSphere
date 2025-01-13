dcec85c4f7616a998c0e796bafb1767780edd6c9
unknown <experte001@gmail.com>
2025-01-14 06:44:35 +0900

    Resolve Suspense Boundary Error🔨

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,13 +2,11 @@
 
 import Form from "@components/Form";
 import { useRouter, useSearchParams } from "@node_modules/next/navigation";
-
-import { useEffect, useState } from "react";
-import { Suspense } from "react";
+import { useEffect, useState, Suspense } from "react";
 
 const EditPrompt = () => {
   const router = useRouter();
-  const [submitting, setSubmitting] = useState();
+  const [submitting, setSubmitting] = useState(false);
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
   const [post, setPost] = useState({
@@ -18,21 +16,26 @@ const EditPrompt = () => {
 
   useEffect(() => {
     const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
-
-      setPost({ prompt: data.prompt, tag: data.tag });
+      if (promptId) {
+        const response = await fetch(`/api/prompt/${promptId}`);
+        const data = await response.json();
+        setPost({ prompt: data.prompt, tag: data.tag });
+      }
     };
-    if (promptId) getPromptDetails();
+    getPromptDetails();
   }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
     if (!promptId) return alert("Prompt ID not found!");
+
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
           prompt: post.prompt,
           tag: post.tag,
@@ -41,16 +44,18 @@ const EditPrompt = () => {
 
       if (response.ok) {
         router.push("/");
+      } else {
+        throw new Error("Failed to update prompt");
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     } finally {
       setSubmitting(false);
     }
   };
 
   return (
-    <Suspense fallback={"Loading..."}>
+    <Suspense fallback={<div>Loading...</div>}>
       <Form
         type="Edit"
         post={post}
