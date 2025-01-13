174c41d25cb2599aedb1ed82c87273d60029c36a
unknown <experte001@gmail.com>
2025-01-13 22:36:22 +0900

    Implementing and Designing Prompt Cart 🛠

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import PromptCart from "@components/PromptCart";
+
+function PromptCartList({ data, handletagClick }) {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCart
+          key={post._id}
+          post={post}
+          handleTagClick={handletagClick}
+        />
+      ))}
+    </div>
+  );
+}
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCartList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
