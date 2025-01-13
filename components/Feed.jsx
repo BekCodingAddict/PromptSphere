e5e2a265d3e21cde41e53282614653a645b620db
unknown <experte001@gmail.com>
2025-01-14 05:32:56 +0900

    Implementing Search Prompts 🔎

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,14 +3,14 @@
 import { useEffect, useState } from "react";
 import PromptCart from "@components/PromptCart";
 
-function PromptCartList({ data, handletagClick }) {
+function PromptCartList({ data, handleTagClick }) {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
         <PromptCart
           key={post._id}
           post={post}
-          handleTagClick={handletagClick}
+          handleTagClick={handleTagClick}
         />
       ))}
     </div>
@@ -21,7 +21,28 @@ const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {};
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      const searchedData = data.filter((prompt) => {
+        return (
+          prompt.prompt.includes(searchText) || prompt.tag.includes(searchText)
+        );
+      });
+
+      setPosts(searchedData);
+    };
+
+    setTimeout(() => {
+      fetchPosts();
+    }, 1000);
+  }, [searchText]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -44,7 +65,7 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCartList data={posts} handleTagClick={() => {}} />
+      <PromptCartList data={posts} handleTagClick={setSearchText} />
     </section>
   );
 };
